c0eafec16c9d87507a982342185ea89c7293555d
ntrrg <ntrrgx@gmail.com>
2015-07-12 20:00:21 -0430

    Archivo con datos predefinidos para la BD

---
+++ bd/data.sql
@@ -0,0 +1,2 @@
+insert into persona values('1234', 'Nombre', null, 'Apellido', null, 'm', 'correo@servidor.com', 'Calle #123, Ciudad edo. Estado', '04XX-1234567', '02XX-1234567');
+insert into usuario values('1234', '81dc9bdb52d04dc20036dbd8313ed055', '1234', null, 1);
